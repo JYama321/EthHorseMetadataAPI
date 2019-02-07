bda4ff75f8392367c15ae0af331661e4f2019fce
山本次郎 <yamajiiii54@gmail.com>
2019-02-07 18:24:29 +0900

    add storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/token/:id', to: 'tokenmetadata#tokeninfo'
-  get '/token/:id/image', to: 'tokenmetadata#token_image'
+  get '/token/:id/image/:gene', to: 'tokenmetadata#token_image'
 end
