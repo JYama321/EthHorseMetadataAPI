a6f809e8bb958f4626ef8e02c907cabd3f5a20fb
山本次郎 <yamajiiii54@gmail.com>
2019-03-14 13:44:46 +0900

    add icon method

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/token/:id', to: 'tokenmetadata#tokeninfo'
   get '/token/:id/image/:gene', to: 'tokenmetadata#token_image'
+  get '/icon', to: 'tokenmetadata#icon'
 end
